e3c294125e42b3ca58149543ef6d8b4c9b941b51
Philip Blom <pblom@lanl.gov>
2025-01-09 07:59:58 -0700

    initial changes

--- setup.py
+++ setup.py
@@ -3,17 +3,24 @@
 from distutils.core import setup
 
 setup(
-    name = "infraga",
+    name = "infraga2",
     license='LANL-MIT',
-    version = '1.0.3',
+    version = '1.1.0',
     description = "A tool for modeling the propagation of infrasound in the limit of geometric acoustics (Python interface).",
     keywords=['infrasound', 'geophysics', 'seismic', 'array'],
     author = "LANL Seismoacoustics Infrasound (LANL-SA) Team",
     author_email = 'pblom@lanl.gov',
 
-    packages = ['infraga'],
+    packages = ['infraga',
+                'infraga.cli',
+                'infraga.doc',
+                'infraga.resources',
+                'infraga.src',
+                'infraga.src.atmo',
+                'infraga.src.geoac',
+                'infraga.src.util'],
 
-    entry_points = {'console_scripts':['infraga=infraga.__main__:main']},
+    entry_points = {'console_scripts':['infraga=infraga.cli.__main__:main']},
 
     install_requires = ['cartopy',
                         'click',
