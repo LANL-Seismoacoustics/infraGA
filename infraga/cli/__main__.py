739321b348bbd2e08bb2dfe7ba33a4a5bd14658f
Phil <pblom@lanl.gov>
2026-05-04 14:26:08 -0600

    normal vector and terrain methods working with ETOPO 2022 update and option for custom terrain files

--- infraga/cli/__main__.py
+++ infraga/cli/__main__.py
@@ -92,7 +92,7 @@ plot.add_command(plotting.plot_map)
 plot.add_command(plotting.plot_animation)
 
 utils.add_command(infraga_utils.build_g2s_grid)
-utils.add_command(infraga_utils.extract_ecmwf)
+#utils.add_command(infraga_utils.extract_ecmwf)
 utils.add_command(infraga_utils.extract_terrain)
 utils.add_command(infraga_utils.nearby_arrivals)
 utils.add_command(infraga_utils.normal_projection)
